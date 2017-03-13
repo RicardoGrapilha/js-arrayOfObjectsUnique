fe9a6a5eb8a157809053f906af2dc4ed6498e5f3
ricardo <ricardo@localhost.localdomain>
2017-03-13 14:36:54 -0300

    separando funcoes e dados para teste

---
+++ js/utils.js
@@ -0,0 +1,52 @@
+//Fucking function to compare objects
+Object.compare = function (obj1, obj2) {
+	//Loop through properties in object 1
+	for (var p in obj1) {
+		//Check property exists on both objects
+		if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false;
+
+		switch (typeof (obj1[p])) {
+			//Deep compare objects
+			case 'object':
+				if (!Object.compare(obj1[p], obj2[p])) return false;
+				break;
+			//Compare function code
+			case 'function':
+				if (typeof (obj2[p]) == 'undefined' || (p != 'compare' && obj1[p].toString() != obj2[p].toString())) return false;
+				break;
+			//Compare values
+			default:
+				if (obj1[p] != obj2[p]) return false;
+		}
+	}
+
+	//Check object 2 for any extra properties
+	for (var p in obj2) {
+		if (typeof (obj1[p]) == 'undefined') return false;
+	}
+	return true;
+}
+function insert_array_html(array_insert, id){
+    var items = document.getElementById(id);
+
+    for (var i = 0; i < array_insert.length; i++ ) {
+        var item = document.createElement("li");
+        item.setAttribute("class" , "list-group-item");
+        item.innerHTML = "<strong>" + array_insert[i].name + "</strong>" + " - "+array_insert[i].type ;
+        items.appendChild(item);
+    }
+}
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status);
+      }
+    };
+    xhr.send();
+};
